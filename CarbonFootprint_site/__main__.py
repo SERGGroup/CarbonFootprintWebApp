86e2399225486ba580fe6795797fbc9b7e99f6e6
Pietro Ungar <pietro.ungar@unifi.it>
2023-08-11 12:29:00 +0200

    __init__.py added

--- CarbonFootprint_site/__main__.py
+++
@@ -1,4 +0,0 @@
-from carbon import app
-from waitress import serve
-
-serve(app, host="localhost", port=5000)
--- CarbonFootprint_site/carbon.py
+++ CarbonFootprint_site/carbon.py
@@ -5,96 +5,100 @@ import pandas as pd
 import os
 
 
-app = Flask(__name__)
+def create_app():
 
+    app = Flask(__name__)
 
-@app.route('/')
-def home():
+    @app.route('/')
+    def home():
 
-    if os.path.isfile('output.csv'):
-        os.remove('output.csv')
+        if os.path.isfile('output.csv'):
+            os.remove('output.csv')
 
-    return render_template('index.html')
+        return render_template('index.html')
 
-@app.route('/home')
-def home1():
-    return render_template('index.html')
+    @app.route('/home')
+    def home1():
+        return render_template('index.html')
 
-@app.route('/results', methods=['POST', 'GET'])
-def result():
+    @app.route('/results', methods=['POST', 'GET'])
+    def result():
 
-    if request.method == 'POST':
+        if request.method == 'POST':
 
-        # rende un dizionario con delle liste contenente tutte le variabili create nell'html
-        html_data = request.form
-        coefficients = get_coefficients(request.form)
-        n_correct_inputs = controlla_ingressi(html_data)
+            # rende un dizionario con delle liste contenente tutte le variabili create nell'html
+            html_data = request.form
+            coefficients = get_coefficients(request.form)
+            n_correct_inputs = controlla_ingressi(html_data)
 
-        if n_correct_inputs == 1:
+            if n_correct_inputs == 1:
 
-            # controlla che sia stato possibile trasformare tutti i dati in float,
-            # altrimenti i dati in ingresso erano sbagliati
+                # controlla che sia stato possibile trasformare tutti i dati in float,
+                # altrimenti i dati in ingresso erano sbagliati
 
-            return """
-                
-                The input data are not correct. 
-                Remember that you can not insert string and float number must use dot
-                
-            """
+                return """
+                    
+                    The input data are not correct. 
+                    Remember that you can not insert string and float number must use dot
+                    
+                """
 
-        prodotto = calcola_output(n_correct_inputs, coefficients)
+            prodotto = calcola_output(n_correct_inputs, coefficients)
 
-        # creo data Frame dei valori co2 per ogni categoria
-        df_totale = pd.DataFrame(prodotto, index=[0])
-        lista_df = dividi_df(df_totale)
+            # creo data Frame dei valori co2 per ogni categoria
+            df_totale = pd.DataFrame(prodotto, index=[0])
+            lista_df = dividi_df(df_totale)
 
-        # lista dei totali della co2 per macroarea
-        output = calcola_tot(lista_df)
+            # lista dei totali della co2 per macroarea
+            output = calcola_tot(lista_df)
 
-        # questa funzione crea il file excel/crea il file partendo dal template già caricato in cartella
-        write_results(prodotto)
+            # questa funzione crea il file excel/crea il file partendo dal template già caricato in cartella
+            write_results(prodotto)
 
-        # questa funzione genera i grafici da mostrare con i risultati
-        crea_grafici(lista_df)
+            # questa funzione genera i grafici da mostrare con i risultati
+            crea_grafici(lista_df)
 
-        # salvo su un file csv i dati output
-        output_df = pd.DataFrame([output], columns=['dieta', 'trasporti', 'casa', 'abbigliamento', 'totale'])
+            # salvo su un file csv i dati output
+            output_df = pd.DataFrame([output], columns=['dieta', 'trasporti', 'casa', 'abbigliamento', 'totale'])
 
-        # salvo il database
-        output_df.to_csv('output.csv')
+            # salvo il database
+            output_df.to_csv('output.csv')
 
-        return render_template('results.html', output=output)
-      
-    elif os.path.isfile('output.csv'):
+            return render_template('results.html', output=output)
 
-        output = list()
-        output_df = pd.read_csv('output.csv', index_col=0)
+        elif os.path.isfile('output.csv'):
 
-        output.append(output_df.at[0, 'dieta'])
-        output.append(output_df.at[0, 'trasporti'])
-        output.append(output_df.at[0, 'casa'])
-        output.append(output_df.at[0, 'abbigliamento'])
-        output.append(output_df.at[0, 'totale'])
+            output = list()
+            output_df = pd.read_csv('output.csv', index_col=0)
 
-        return render_template('results.html', output=output)
+            output.append(output_df.at[0, 'dieta'])
+            output.append(output_df.at[0, 'trasporti'])
+            output.append(output_df.at[0, 'casa'])
+            output.append(output_df.at[0, 'abbigliamento'])
+            output.append(output_df.at[0, 'totale'])
 
-    else:
+            return render_template('results.html', output=output)
 
-        return 'First insert your data and calculate your carbon footprint!'
-        
-@app.route('/download')
-def download():
-    
-    # Percorso del file Excel da scaricare
-    file_path = os.path.join(MAIN_DIR, 'results_download.xlsx')
+        else:
 
-    # Invia il file Excel al client come allegato
-    return send_file(file_path, as_attachment=True)
+            return 'First insert your data and calculate your carbon footprint!'
+
+    @app.route('/download')
+    def download():
+
+        # Percorso del file Excel da scaricare
+        file_path = os.path.join(MAIN_DIR, 'results_download.xlsx')
+
+        # Invia il file Excel al client come allegato
+        return send_file(file_path, as_attachment=True)
+
+    return app
 
 
 if __name__ == '__main__':
 
-   app.run(debug=True)
+    curr_app = create_app()
+    curr_app.run(debug=True)
 
     
 
