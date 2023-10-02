4b2c5b07db1bad10f785e38ff7ec12117e7cb806
Pietro Ungar <pietro.ungar@unifi.it>
2023-10-02 11:30:16 +0200

    V0.2.0 - Modifiche Ragazzi

--- CarbonFootprint_site/carbon.py
+++ CarbonFootprint_site/carbon.py
@@ -1,104 +1,131 @@
-from .function import calcola_tot, crea_grafici, controlla_ingressi,dividi_df, calcola_output, write_results
-from .constants import MAIN_DIR,  IMAGES_DIR, get_coefficients
 from flask import Flask, render_template, request, send_file
+from function import calcola_tot, crea_grafici, controlla_ingressi,dividi_df, calcola_output, write_results
 import pandas as pd
 import os
+app = Flask(__name__)
 
 
-def create_app():
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-    app = Flask(__name__)
+@app.route('/results',methods = ['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+          dati_html=request.form #rende un dizionario con delle liste contenente tutte le variabili create nell'html
+          coefficienti={
+              #alimentazione
+              'beef_herd':68.25/7/1000,#kgCO2/kg
+              'lumb_mutton':24/7/1000,#kgCO2/kg
+              'cheese':21/7/1000,#kgCO2/kg
+              'beef_dairy':44.75/7/1000,#kgCO2/kg
+              'chocolate':19/7/1000,#kgCO2/kg
+              'coffee':0.05953/7,#kgCO2/cup
+              'prawns_farmed':12/7/1000,#kgCO2/kg
+              'pig_meat':7.9/7/1000,#kgCO2/kg
+              'wheat_rye':1.4/7/1000,#kgCO2/kg
+              'tomatoes':1.4/7/1000,#kgCO2/kg
+              'maize':1/7/1000,#kgCO2/kg
+              'peas':0.9/7/1000,#kgCO2/kg
+              'soy_milk':0.9/7/1000,#kgCO2/kg
+              'poultry_meat':10.4/7/1000,#kgCO2/kg
+              'olive_oil':6/7/1000,#kgCO2/kg
+              'fish_farmed':5/7/1000,#kgCO2/kg
+              'pasta':1.12/7/1000, #kgCO2/kg
+              'bread':0.731/7/1000, #kgCO2/kg
+              'eggs':4.5/20/7,#kgCO2/kg /20 perchè un uovo è circa 50g
+              'rice':4/7/1000,#kgCO2/kg
+              'fish_wildcatch':3/7/1000,#kgCO2/kg
+              'milk':3/7/1000,#kgCO2/kg
+              'cane_sugar':3/7/1000,#kgCO2/kg
+              'groundnuts':2.5/7/1000,#kgCO2/kg
+              'bananas':0.7/7/5,#kgCO2/kg
+              'root_vegetables':0.4/7/1000,#kgCO2/kg
+              'apples':0.4/7/4,#kgCO2/kg- una mela pesa 250g
+              'citrus_fruit':0.3/7/4,#kgCO2/kg
+              'nuts':0.3/7/1000,#kgCO2/kg
+              #trasporti
+              'domestic_flight':0.255/365,
+              'mediumcar_petrol':0.269/7/float(request.form['passangers_petrol']),
+              'mediumcar_diesel':0.229/7/float(request.form['passangers_diesel']),
+              'short_flight':0.156/365,
+              'long_flight':0.150/365,
+              'bus':0.093/7,
+              'motorcycle':0.0968/7,
+              'electric_vehicle':0.12/7/float(request.form['passangers_electric']),
+              'national_rail':0.041/7,
+              'ferry':0.019/7,
+              'eurostar':0.006/7,
+              #casa
+              'refrigerator':0.14/float(request.form['number_family']), #kgco2/day
+              'food_cooking':0.05048/float(request.form['number_family']), #kgco2/meal
+              'oven':0.7091/7/float(request.form['number_family']), #kgco2/meal
+              'washing_machine':0.6283/7/float(request.form['number_family']), #kgco2/cycle
+              'shower':0.6057/7,#kgco2/shower
+              'laptop':100/365,
+              'smartphone':120/365,
+              'heating':0.19*float(request.form['efficiency_class'])/float(request.form['number_family'])/365, #from user it returns directly the number of co2
+              'cooling':0.2605*float(request.form['cooling_power'])*float(request.form['hot_days'])/float(request.form['number_family'])/365,
+              #abbigliamento
+              'cotton_shirt':20*0.150/365, #kgco2/unit
+              'cotton_sweatjacket':20*0.55/365, #kgco2/unit
+              'acrylic_jacket':35.7*0.55/365, #kgco2/unit
+              'woolen_sweater':13.12/365, #kgco2/unit
+              'polyester_shirt':27.2*0.15/365, #kgco2/unit
+              'jeans':33.4/365 , #kgco2/unit
+              }
+          ingressi_corretti=controlla_ingressi(dati_html)
+          if ingressi_corretti==1: #controlla che sia stato possibile trasformare tutti i dati in float, altrimenti i dati in ingresso erano sbagliati
+              return 'The input data are not correct. Remember that you can not insert string and float number must use dot'
+          prodotto=calcola_output(ingressi_corretti, coefficienti)
+          df_totale=pd.DataFrame(prodotto,index=[0])#creo data Frame dei valori co2 per ogni categoria
+          lista_df=dividi_df(df_totale)
+          output=calcola_tot(lista_df) #lista dei totali della co2 per macroarea
+          write_results(prodotto) #questa funzione crea il file excel/crea il file partendo dal template già caricato in cartella
+          try:    
+              crea_grafici(lista_df) #funzione che crea grafici e li salva sulla cartella static
+          except:#se non riesce a fare la funzione crea_grafici() cancella i grafici se esistono già
+             if os.path.isfile('./static/images/grafico_abbigliamento.png'):
+                 os.remove('./static/images/grafico_abbigliamento.png')
+             if os.path.isfile('./static/images/grafico_trasporti.png'):
+                 os.remove('./static/images/grafico_trasporti.png')
+             if os.path.isfile('./static/images/grafico_dieta.png'):
+                 os.remove('./static/images/grafico_dieta.png')
+             if os.path.isfile('./static/images/grafico_casa.png'):
+                 os.remove('./static/images/grafico_casa.png')
+             if os.path.isfile('./static/images/grafico_totale.png'):
+                 os.remove('./static/images/grafico_totale.png')
+             if os.path.isfile('./static/images/grafico_comparativa.png'):
+                os.remove('./static/images/grafico_comparativa.png')
 
-    @app.route('/')
-    def home():
+          return render_template('results.html',output= output)
 
-        if os.path.isfile('output.csv'):
-            os.remove('output.csv')
+    else:
+        return 'First insert your data and calculate your carbon footprint!'
+        
+@app.route('/download')
+def download():
+    
+    # Percorso del file Excel da scaricare
+    file_path = 'results_download.xlsx'
 
-        return render_template('index.html')
+    # Invia il file Excel al client come allegato
+    return send_file(file_path, as_attachment=True)
 
-    @app.route('/home')
-    def home1():
-        return render_template('index.html')
+@app.route('/explanation')
+def explanation():
+    return render_template('how-it-works.html')
 
-    @app.route('/results', methods=['POST', 'GET'])
-    def result():
+@app.route('/references')
+def download_ref():
+    
+    # Percorso del file Excel da scaricare
+    file_path = 'references_download.xlsx'
 
-        if request.method == 'POST':
-
-            # rende un dizionario con delle liste contenente tutte le variabili create nell'html
-            html_data = request.form
-            coefficients = get_coefficients(request.form)
-            n_correct_inputs = controlla_ingressi(html_data)
-
-            if n_correct_inputs == 1:
-
-                # controlla che sia stato possibile trasformare tutti i dati in float,
-                # altrimenti i dati in ingresso erano sbagliati
-
-                return """
-                    
-                    The input data are not correct. 
-                    Remember that you can not insert string and float number must use dot
-                    
-                """
-
-            prodotto = calcola_output(n_correct_inputs, coefficients)
-
-            # creo data Frame dei valori co2 per ogni categoria
-            df_totale = pd.DataFrame(prodotto, index=[0])
-            lista_df = dividi_df(df_totale)
-
-            # lista dei totali della co2 per macroarea
-            output = calcola_tot(lista_df)
-
-            # questa funzione crea il file excel/crea il file partendo dal template già caricato in cartella
-            write_results(prodotto)
-
-            # questa funzione genera i grafici da mostrare con i risultati
-            crea_grafici(lista_df)
-
-            # salvo su un file csv i dati output
-            output_df = pd.DataFrame([output], columns=['dieta', 'trasporti', 'casa', 'abbigliamento', 'totale'])
-
-            # salvo il database
-            output_df.to_csv('output.csv')
-
-            return render_template('results.html', output=output)
-
-        elif os.path.isfile('output.csv'):
-
-            output = list()
-            output_df = pd.read_csv('output.csv', index_col=0)
-
-            output.append(output_df.at[0, 'dieta'])
-            output.append(output_df.at[0, 'trasporti'])
-            output.append(output_df.at[0, 'casa'])
-            output.append(output_df.at[0, 'abbigliamento'])
-            output.append(output_df.at[0, 'totale'])
-
-            return render_template('results.html', output=output)
-
-        else:
-
-            return 'First insert your data and calculate your carbon footprint!'
-
-    @app.route('/download')
-    def download():
-
-        # Percorso del file Excel da scaricare
-        file_path = os.path.join(MAIN_DIR, 'results_download.xlsx')
-
-        # Invia il file Excel al client come allegato
-        return send_file(file_path, as_attachment=True)
-
-    return app
+    # Invia il file Excel al client come allegato
+    return send_file(file_path, as_attachment=True)
 
 
 if __name__ == '__main__':
 
-    curr_app = create_app()
-    curr_app.run(debug=True)
-
-    
-
+    app.run(debug=True)
--- CarbonFootprint_site/function.py
+++ CarbonFootprint_site/function.py
@@ -1,166 +1,121 @@
-from .constants import MAIN_DIR,  IMAGES_DIR
-import matplotlib.pyplot as plt
+#here are defined all functions usefull into carbon.py
+
 import pandas as pd
-import numpy as np
-import functools
 import openpyxl
-import os
-
-
-DEFAULT_OTHER_LIMIT = 0.04
-
+import matplotlib.pyplot as plt
 
 def crea_grafici(lista_df):
-
-    # Questa parte fino al cancelletto serve per riscrivere
-    # il df in modo che il programma riesca a fare il grafico
-
-    # Calcola i totali di ogni macroarea
-    lista_tot = calcola_tot(lista_df)[0:4]
-
-    # Percentuale sotto la quale la categoria
-    # viene contrassegnata come 'altro'
-    perc = 0.04
-
-    # Create the category graph
-    plot_names = ["dieta", "trasporti", "casa", "abbigliamento"]
-
-    for i in range(len(plot_names)):
-
-        __plot_data_frame_category(lista_df[i], plot_names[i], perc=perc)
-
-    # Create the overall graph
-    __plot_data_frame_overall(lista_tot, 'totale')
-
-    #costruzione del grafico a barre che confronta la cf personale con le medie mondiali
-    __plot_data_frame_comparison(lista_tot, 'comparativa')
-
-def __check_plot_creation(function_in):
-
-    @functools.wraps(function_in)
-    def decorated_func(df_in, df_name, perc=DEFAULT_OTHER_LIMIT):
-
-        try:
-
-            function_in(df_in, df_name, perc=perc)
-
-        except:
-
-            graph_path = __get_graph_path(df_name)
-
-            if os.path.isfile(graph_path):
-                os.remove(graph_path)
-
-    return decorated_func
-
-@__check_plot_creation
-def __plot_data_frame_category(df_in, df_name, perc=DEFAULT_OTHER_LIMIT):
-
-    # get the column indices
-    keys = list(df_in.columns)
-
-    values_list = list()
-    index_list = list()
-    other = 0
-
-    for i in keys:
-
-        # If the category does not reach the 'perc' level of impact (default is 4%)
-        # it is automatically moved in the 'other' category
-
-        if df_in.at[0, i] / np.array(df_in).sum() < perc:
-
-            other += df_in.at[0, i]
-
+    #questa parte fino al cancelletto serve per riscrivere il df in modo che il programma riesca a fare il grafico
+    lista_tot=calcola_tot(lista_df)[0:4] #calcola i totali di ogni macroarea
+    perc=0.04 #percentuale altro
+    df_dieta=lista_df[0]
+    chiavi_dieta=list(df_dieta.columns) #prendo i nomi delle colonne
+    valori_dieta=[]
+    indici=[]
+    altro=0
+    for i in chiavi_dieta:
+        if df_dieta.at[0,i]/lista_tot[0]<perc: #ciclo che controlla che il contributo sia superiore al 7% del totale, sennò viene assegnato alla categoria altro
+            altro+=df_dieta.at[0,i]
         else:
-
-            # If its impact is > than 'perc', the program insert the data in the lists
-            values_list.append(df_in.at[0, i])
-            index_list.append(i)
-
-    if other > 0:
-
-        values_list.append(other)
-        index_list.append('others')
-
-    # Plot and Save graph
-    df_graph = pd.DataFrame({df_name: values_list}, index=index_list)
-    graph = df_graph.plot(kind='pie', y=df_name, label='', legend='', figsize=(5, 5,))
-    graph.figure.savefig(__get_graph_path(df_name))
-
-@__check_plot_creation
-def __plot_data_frame_overall(lista_tot, df_name, perc):
-
-    df_graf_tot = pd.DataFrame({
-
-        df_name: lista_tot
-
-    },
-
-        index=[
-
-            'Diet',
-            'Transport',
-            'Home',
-            'Clothes'
-
-        ]
-
-    )
-
-    graf_tot = df_graf_tot.plot(kind='pie', y=df_name, label='', legend='', figsize=(5, 5,))
-    graf_tot.figure.savefig(__get_graph_path(df_name))
-
-@__check_plot_creation
-def __plot_data_frame_comparison(lista_tot, df_name, perc):
-
-    impact_capita = {
-
-        'Europe': 7.11 * 1000, 'Asia': 4.62 * 1000, 'Italy': 5.55 * 1000, 'Bangladesh': 0.55 * 1000,
-        'France': 4.74 * 1000, 'Germany': 8.09 * 1000, 'Spain': 4.92 * 1000, 'United States': 14.86 * 1000,
-        'World': 4.69 * 1000, 'You': sum(lista_tot) * 365
-
-    }
-
-    grafico_comp = pd.DataFrame(impact_capita, index=[0])
-    grafico_comparativa = grafico_comp.plot(kind='bar', figsize=(6, 6,))
+         valori_dieta.append(df_dieta.at[0,i]) #prende ogni elemento nel df riga 0 colonna i(nome colonna)
+         indici.append(i)
+    valori_dieta.append(altro)
+    df_dieta_grafico=pd.DataFrame({'dieta':valori_dieta},index=indici+['others'])
+    #
+    graf_dieta=df_dieta_grafico.plot(kind='pie',y='dieta',label='',legend='',figsize=(5,5,))
+    graf_dieta.figure.savefig('./static/images/grafico_dieta.png') #funzione per salvare il grafico nella cartella
+    
+    df_trasporti=lista_df[1]
+    chiavi_trasporti=list(df_trasporti.columns) #prendo i nomi delle colonne
+    valori=[]
+    indici=[]
+    altro=0
+    for i in chiavi_trasporti:
+        if df_trasporti.at[0,i]/lista_tot[1]<perc: #ciclo che controlla che il contributo sia superiore al 7% del totale, sennò viene assegnato alla categoria altro
+            altro+=df_trasporti.at[0,i]
+        else:
+            valori.append(df_trasporti.at[0,i]) #prende ogni elemento nel df riga 0 colonna i(nome colonna)
+            indici.append(i)
+    valori.append(altro)
+    indici.append('others')
+    df_trasporti_grafico=pd.DataFrame({'trasporti':valori},index=indici)
+    graf_trasporti=df_trasporti_grafico.plot(kind='pie',y='trasporti',label='',legend='', figsize=(5,5,))
+    graf_trasporti.figure.savefig('./static/images/grafico_trasporti.png')
+    
+    df_casa=lista_df[2]
+    chiavi_casa=list(df_casa.columns) #prendo i nomi delle colonne
+    valori=[]
+    indici=[]
+    altro=0
+    for i in chiavi_casa:
+        if df_casa.at[0,i]/lista_tot[2]<perc: #ciclo che controlla che il contributo sia superiore al 7% del totale, sennò viene assegnato alla categoria altro
+            altro+=df_casa.at[0,i]
+        else:
+            valori.append(df_casa.at[0,i]) #prende ogni elemento nel df riga 0 colonna i(nome colonna)
+            indici.append(i)
+    valori.append(altro)
+    indici.append('others')
+    df_casa_grafico=pd.DataFrame({'casa':valori},index=indici)
+    graf_casa=df_casa_grafico.plot(kind='pie',y='casa',label='',legend='',figsize=(5,5,))
+    graf_casa.figure.savefig('./static/images/grafico_casa.png')
+    
+    df_abbigliamento=lista_df[3]
+    chiavi_abbigliamento=list(df_abbigliamento.columns) #prendo i nomi delle colonne
+    valori=[]
+    indici=[]
+    altro=0
+    for i in chiavi_abbigliamento:
+        if df_abbigliamento.at[0,i]/lista_tot[3]<perc: #ciclo che controlla che il contributo sia superiore al 7% del totale, sennò viene assegnato alla categoria altro
+            altro+=df_abbigliamento.at[0,i]
+        else:
+            valori.append(df_abbigliamento.at[0,i]) #prende ogni elemento nel df riga 0 colonna i(nome colonna)
+            indici.append(i)
+    valori.append(altro)
+    indici.append('others')
+    df_abbigliamento_grafico=pd.DataFrame({'abbigliamento':valori},index=indici)
+    graf_abbigliamento=df_abbigliamento_grafico.plot(kind='pie',y='abbigliamento',label='',legend='',figsize=(5,5,))
+    graf_abbigliamento.figure.savefig('./static/images/grafico_abbigliamento.png')
+    
+    df_graf_tot=pd.DataFrame({'riepilogo':lista_tot},index=['Diet','Transport','Home','Clothes']) #prende la lista sopra e crea df
+    graf_tot=df_graf_tot.plot(kind='pie',y='riepilogo',label='',legend='',figsize=(5,5,))
+    graf_tot.figure.savefig('./static/images/grafico_totale.png')
+    
+    #costruzione del grafico a barre che confronta la cf personale con le medie mondiali
+    impact_capita={'Europe':7.11*1000,
+                   'Asia':4.62*1000,
+                   'Italy':5.55*1000,
+                   'Bangladesh': 0.55*1000,
+                   'France': 4.74*1000,
+                   'Germany': 8.09*1000,
+                   'Spain': 4.92*1000,
+                   'United States':14.86*1000,
+                   'World':4.69*1000,
+                   }
+    impact_capita['You']=sum(lista_tot)*365
+    grafico_comp=pd.DataFrame(impact_capita, index=[0])
+    grafico_comparativa=grafico_comp.plot(kind='bar',figsize=(6,6,))
     plt.ylabel('kgCO2 per capita/year')
+    grafico_comparativa.figure.savefig('./static/images/grafico_comparativa.png')
+    
+    
+    return
 
-    grafico_comparativa.figure.savefig(__get_graph_path(df_name))
-
-def __get_graph_path(df_name):
-
-    return os.path.join(IMAGES_DIR, 'grafico_{}.png'.format(df_name))
-
-def controlla_ingressi(ingressi):
-
-    #nota bene che i dizionari hanno gli stessi indici
+def controlla_ingressi(ingressi): #nota bene che i dizionari hanno gli stessi indici
     #funzione che riscrive gli ingressi in un nuovo dizionario e controlla che i dati inseriti siano corretti
     #se i dati inseriti non sono corretti rende 1
     #se gli input sono corretti rende il dizionario 'input_corretti'
     #devono essere 
-
     chiavi=list(ingressi.keys()) #lista delle chiavi del dizionario
     input_corretti={}
-
     for i in chiavi:
-
         if ingressi[i]=='':
-
             quantita=0.0
-
         else:
-
-            try:
-
+            try:    
                 quantita=float(ingressi[i])
-
             except ValueError: #se la funzione float non è applicabile
-
                 return 1
-
         input_corretti[i]=quantita #cosi converto gli elementi vuoti in zeri e tutti in float
-
     return input_corretti
 
 def calcola_output(ingressi, coefficienti): 
@@ -189,6 +144,8 @@ def dividi_df(df_totale):
             casa=True
         elif i=='cotton_shirt':
             abbigliamento=True
+        
+        
         
         if (not trasporti and not casa and not abbigliamento):
             diz_dieta[i]=[df_totale.at[0,i]]
@@ -242,18 +199,16 @@ def calcola_tot(lista_df):
      
      return [tot_dieta,tot_trasporti,tot_casa,tot_abbigliamento,tot]
 
-def write_results(prodotti):
 
-    count = 1
-    file = openpyxl.load_workbook('template_results.xlsx')
-    sheet = file['Sheet3']
-    chiavi = list(prodotti.keys())
-
-    for i in chiavi:
-
-        tcount = str(count)
-        sheet['A'+tcount] = i
-        sheet['B'+tcount] = prodotti[i]
-        count = count + 1
-
-    file.save('results_download.xlsx')
+def write_results (prodotti):
+	count=1
+	file=openpyxl.load_workbook('template_results.xlsx')
+	sheet=file['Sheet3']
+	chiavi=list(prodotti.keys())
+	for i in chiavi:
+		tcount=str(count)
+		sheet['A'+tcount]=i
+		sheet['B'+tcount]=prodotti[i]
+		count=count+1
+	file.save('results_download.xlsx')
+	return
