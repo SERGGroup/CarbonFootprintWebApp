1744c754afc69ad1a2829f7b6c1901b04608baa2
Pietro Ungar <pietro.ungar@unifi.it>
2023-11-23 15:44:47 +0100

    V0.3.0 - HTML Rewritten

--- CarbonFootprint_site/carbon.py
+++ CarbonFootprint_site/carbon.py
@@ -3,42 +3,46 @@ from .python_code.form_definition_class import MainFormClass
 from .python_code.support.constants import EXCEL_DIR
 import os
 
-app = Flask(__name__)
+def create_app():
 
+    app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
-def home():
+    @app.route('/', methods=['POST', 'GET'])
+    def home():
 
-    main_class = MainFormClass()
-    curr_form = main_class.form_class(request.form)
+        main_class = MainFormClass()
+        curr_form = main_class.form_class(request.form)
 
-    if request.method == 'POST' and curr_form.validate():
+        if request.method == 'POST' and curr_form.validate():
 
-        main_class.evaluate_results(curr_form)
-        return render_template('results.html', main_class=main_class)
+            main_class.evaluate_results(curr_form)
+            return render_template('results.html', main_class=main_class)
 
-    return render_template('support/form_layout.html', form=curr_form, main_class=main_class)
+        return render_template('support/form_layout.html', form=curr_form, main_class=main_class)
 
-@app.route('/download')
-def download():
-    
-    # Percorso del file Excel da scaricare
-    file_path = 'results_download.xlsx'
+    @app.route('/download')
+    def download():
 
-    # Invia il file Excel al client come allegato
-    return send_file(file_path, as_attachment=True)
+        # Percorso del file Excel da scaricare
+        file_path = 'results_download.xlsx'
 
-@app.route('/explanation')
-def explanation():
-    return render_template('how-it-works.html')
+        # Invia il file Excel al client come allegato
+        return send_file(file_path, as_attachment=True)
 
-@app.route('/references')
-def download_ref():
-    
-    file_path = os.path.join(EXCEL_DIR, 'references_download.xlsx')
-    return send_file(file_path, as_attachment=True)
+    @app.route('/explanation')
+    def explanation():
+        return render_template('how-it-works.html')
+
+    @app.route('/references')
+    def download_ref():
+
+        file_path = os.path.join(EXCEL_DIR, 'references_download.xlsx')
+        return send_file(file_path, as_attachment=True)
+
+    return app
 
 
 if __name__ == '__main__':
 
-    app.run(debug=True)
+    curr_app = create_app()
+    curr_app.run(debug=True)
